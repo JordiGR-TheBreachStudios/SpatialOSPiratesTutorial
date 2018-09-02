c0964fdc2f3e1d77d9f3e23f7f876f2f5625a20b
Jordi Guerrero <jordi.guerrero@yahoo.com>
2018-09-02 23:28:43 +0200

    [CppWorkerTutorial] Implemented step 3 of lesson 3.

--- workers/PirateShipMovement/src/startup.cc
+++ workers/PirateShipMovement/src/startup.cc
@@ -7,6 +7,13 @@
 #include <improbable/ship/ShipControls.h>
 #include <chrono>
 #include <thread>
+#include <atomic>
+#include <mutex>
+#include <functional>
+
+std::atomic_bool is_connected;
+std::mutex connection_mutex;
+std::mutex view_mutex;
 
 // Enables the nice 5s syntax for time
 using namespace std::chrono_literals;
@@ -43,6 +50,55 @@ std::string get_random_characters(size_t count)
     std::string str(count, 0);
     std::generate_n(str.begin(), count, randchar);
     return str;
+}
+
+template <typename T>
+void thread_safe_component_update(worker::Connection& connection,
+	worker::EntityId entity_id,
+	const typename T::Update& update)
+{
+	std::lock_guard<std::mutex> lock(connection_mutex);
+	connection.SendComponentUpdate<T>(entity_id, update);
+}
+
+void steering_update(worker::Connection& connection,
+	worker::View& view)
+{
+	// Generates random floats between two values
+	auto rand_float = [](float low, float high)
+	{
+		return low + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (high - low)));
+	};
+
+	// Holds each component update before sending it
+	improbable::ship::ShipControls::Update steering_update;
+
+	while (is_connected.load())
+	{
+		// Make sure the view is not modified while iterating
+		// std::shared_mutex could be used in C++17 as we're just reading on this thread
+		view_mutex.lock();
+
+		for (auto iter = view.Entities.begin(); iter != view.Entities.end(); ++iter)
+		{
+			auto entity_id = iter->first;
+
+			// Randomize speed and steering for each ship
+			steering_update.set_target_speed(rand_float(0.0, 1.0));
+
+			// The change in steering is small to make sure ships don't suddenly turn around
+			steering_update.set_target_steering((rand_float(-15.0, 15.0)));
+
+			// Send the steering update to SpatialOS
+			thread_safe_component_update<improbable::ship::ShipControls>(connection, entity_id, steering_update);
+		}
+
+		// Unlock before sleeping
+		view_mutex.unlock();
+
+		// Add a delay between each iteration to avoid excess steering changes
+		std::this_thread::sleep_for(5s);
+	}
 }
 
 // Entry point
@@ -109,7 +165,7 @@ int main(int argc, char** argv)
     connection.SendLogMessage(worker::LogLevel::kInfo, kLoggerName, "Connected successfully");
 
     // Register callbacks and run the worker main loop.
-    bool is_connected = connection.IsConnected();
+    is_connected.store(connection.IsConnected());
 
     // Create a view
     worker::View view{ShipComponents{}};
@@ -123,7 +179,7 @@ int main(int argc, char** argv)
     view.OnDisconnect([&](const worker::DisconnectOp& op)
         {
             std::cerr << "[disconnect] " << op.Reason << std::endl;
-            is_connected = false;
+            is_connected.store(false);
         });
 
     // Print log messages received from SpatialOS
@@ -137,37 +193,27 @@ int main(int argc, char** argv)
             std::cout << "[remote] " << op.Message << std::endl;
         });
 
-    if (is_connected)
+    if (is_connected.load())
     {
         std::cout << "[local] Connected successfully to SpatialOS, listening to ops... " << std::endl;
     }
 
-    // Holds each component update before sending it
-    improbable::ship::ShipControls::Update steering_update;
+	// Start periodic worker jobs
+	std::thread steering_thread(steering_update, std::ref(connection), std::ref(view));
 
-    while (is_connected)
-    {
-        auto ops = connection.GetOpList(kGetOpListTimeoutInMilliseconds);
+	// Run the main worker loop
+	while (is_connected.load())
+	{
+		auto ops = connection.GetOpList(kGetOpListTimeoutInMilliseconds);
 
-        // Process the list of Ops by the view
-        view.Process(ops);
+		// Process the list of ops by the view
+		// You need to make sure no other thread is acessing the map of entities
+		view_mutex.lock();
+		view.Process(ops);
+		view_mutex.unlock();
+	}
 
-        for (auto iter = view.Entities.begin(); iter != view.Entities.end(); ++iter)
-        {
-            auto entity_id = iter->first;
-            // Randomize speed and steering for each ship
-            steering_update.set_target_speed(rand_float(0.0, 1.0));
-
-            // The change in steering is small to make sure ships don't suddenly turn around
-            steering_update.set_target_steering((rand_float(-15.0, 15.0)));
-
-            // Send the steering update to SpatialOS
-            connection.SendComponentUpdate<improbable::ship::ShipControls>(entity_id, steering_update);
-        }
-
-        // Add a delay between each iteration to avoid excess steering changes
-        std::this_thread::sleep_for(5s);
-    }
+	steering_thread.join();
 
     return ErrorExitStatus;
 }
